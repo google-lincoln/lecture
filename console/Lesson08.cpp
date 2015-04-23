dd7ad579d85e1869447ff6038600ba17c2779964
noah_lincoln <linshanying@hotmail.com>
2015-04-23 08:37:11 +0800

    断点设置不对不能调试

--- console/Lesson08.cpp
+++ console/Lesson08.cpp
@@ -1,9 +1,26 @@
 #include "Lesson08.h"
 #include <stdio.h>
+#include <stdlib.h>
 #include "sqlite3.h"
 
 void connect_database()
 {
+    sqlite3* db;
+    char* zErrMsg;
+    int rc;
+
+    rc = sqlite3_open("test.db",&db);
+
+    if(rc!=SQLITE_OK)
+    {
+        fprintf(stderr,"Can't open database: %s\n",sqlite3_errmsg(db));
+        exit(1);
+    }
+    else
+    {
+        fprintf(stderr,"Open database sucessfully\n");
+    }
+    sqlite3_close(db);
     printf("hell world\n");
 
 }
